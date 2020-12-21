4917846ad3199c573aba45674d4dc847627675ba
Сирбиладзе Тамаз <70637893+Mae-lv@users.noreply.github.com>
2020-12-21 20:23:58 +0300

    Add files via upload

---
+++ Queue.java
@@ -0,0 +1,16 @@
+package ru.mirea.pr11;
+
+public interface Queue<E> {
+
+    void enqueue(E e);
+
+    E element();
+
+    E dequeue();
+
+    int size();
+
+    boolean isEmpty();
+
+    void clear();
+}
